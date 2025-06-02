edadfbafdee19c581989659d48dd3235012a1745
Haytham <jesh.suah25@gmail.com>
2025-06-01 19:25:20 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,688 @@
+const lyrics = {
+  "meant_to_be.mp3": `You say you love me, but you just wanna be friends with me
+You say you love me, but your work took all your energy
+You never listen when I just needed an apology
+I read our book, this chapter's closing, now we're history
+
+You just took like twenty thousand hours from my life, so
+I deleted all our pictures off my fucking drive, sorry
+Thank you for this song, you broke my heart and it's alright
+'Cause, if it's meant to be, it's meant to be
+
+Right one, wrong time
+Today's a good day to say goodbye
+I was bad to you, you were worst to me
+But baby we just ain't meant to be
+Fool me once, fool me twice
+Shame on me for the last time
+I was bad to you, you were worst to me
+But baby we just ain't meant to be
+
+Give you all my love and all my trust and my affection
+"Sorry" ain't enough and you say you want more attention
+We was playing games but we was sitting in different sections
+And now I got your stalker and he's clouding all my mentions
+
+You just took like twenty thousand hours from my life, so
+I deleted all our pictures off my fucking drive, sorry
+Thank you for this song, you broke my heart and it's alright
+'Cause, If it's meant to be, it's meant to be
+
+Right one, wrong time
+Today's a good day to say goodbye
+I was bad to you, you were worst to me
+But baby we just ain't meant to be
+Fool me once, fool me twice
+Shame on me for the last time
+I was bad to you, you were worst to me
+But baby we just ain't meant to be
+
+But baby, this is just how it has to be
+But baby, this is just how it has to be`,
+
+  "meant_to_be_Español.mp3":`Dices amarme 
+Pero con tus amigos estás 
+Dices amarme 
+Pero nunca te encuentras aquí 
+Nunca e escuchado 
+Una disculpa saliendo de ti 
+El final de esta historia 
+Ya me contaron a mi 
+Me quitaste miles de 
+Horas de mi vida amando 
+Borre todas las fotos que 
+Tenía aquí guardado 
+Te agradezco está canción
+por averla inspirado 
+Si tenía que ser así 
+Que sea así 
+
+Eras tú 
+Pero el tiempo no 
+Hoy es el día de decir adiós 
+El malo fui aunque todo di 
+Supongo que no eras para mí 
+Mienteme una vez más 
+Me avergüenza el recordar 
+El malo fui 
+Aunque todo di 
+Porque tenía que ser así 
+
+Te pido perdón 
+Si es que no pude ser perfecto 
+Lamento que tuviera 
+Que ver todos mis defectos 
+Jugábamos al juego 
+Del amor sin entendernos 
+Ahora te estalkeo 
+Desde una cuenta en secreto 
+Me quitaste miles de 
+Horas de mi vida amando 
+Borre todas las fotos que 
+Tenía aquí guardado 
+Te agradezco está canción
+Por averla inspirado 
+Si tenía que ser así 
+Que sea así 
+
+
+Eras tú 
+Pero el tiempo no 
+Hoy es el día de decir adiós 
+El malo fui aunque todo di 
+Supongo que no eras para mí 
+Mienteme una vez más 
+Me avergüenza el recordar 
+El malo fui 
+Aunque todo di 
+Porque tenía que ser así
+
+Porque tenía que ser así`,
+
+  "Miss_wanna_die.mp3": `Awake, I opened my eyes to see
+A hospital, so white and clean
+
+I was walking up to the roof, I think
+I can’t recall at all a single thing
+
+Held a hand for me
+You were there, I know
+But couldn’t reach for me
+
+That would fall down all the time
+But stand and reach my hand, you'll see
+Have you tried crossing over yet?
+
+I wanna die, wanna die
+But don’t really wanna die
+You were there, you would care
+Making me aware
+
+Every scar, all the blood
+More and more, they’re never done
+Not enough, not enough
+
+I wanna die, wanna die
+But don’t really wanna die
+If I died, you would cry
+And I don’t know why
+
+Every scar, all the blood
+More and more, they’re never done
+Not enough, not enough
+
+When I forget you, I’m all alone
+Without a place to go
+But then play a mirror like a show
+The past I use to know a dream
+
+Till I see every part of me
+Eyes are red following and
+I wanna live, wanna live
+Deep inside I’ve always been
+Reaching out for a hand
+So don’t let this be the end
+
+Mushrooms growing on my head
+Still remember even then
+Where you are when I’m dead
+
+I wanna die, wanna die
+But don’t really wanna die
+You were there, you would care
+You know it really isn’t fair!
+
+Every time I forgot
+You remain inside my thoughts
+Not enough, not enough
+
+I wanna die, wanna die, wanna die!
+But I still couldn’t die
+With you by my side
+Behind the days I spent alone, afraid
+Now you’re here to stay
+
+And the scars never fade
+Memories that replay
+Rewind, yet again to the pain
+We felt on that day`,
+
+   "Miss_Wanna_Die_Español.mp3":`Me desperté, a mi lado estás
+¿Cómo llegué a este hospital?
+Caminaba sobre el tejado y…
+No puedo recordar lo que pasó
+
+Sé que me intentaste salvar.
+“Toma mano” y aún así, no pudiste alcanzar.
+¿La próxima lo lograrás?
+¿Para mí siempre vas a estar?
+
+Quiero morir… pero si estás, no lo quisiera estar
+Te importé, pude ver, lo agradeceré
+
+Las heridas, la sangre jamás van a cesar.
+No lo harán, no lo harán.
+
+Quiero morir… pero si estás, no lo quisiera estar
+Si lo estoy, llorarás, and i don’t know why 
+
+Las heridas, la sangre jamás van a cesar.
+No lo harán, no lo harán.
+
+
+
+Si a mi lado tú no estás, no sé quién soy ni a donde voy.
+El espejo se rompió, me atormenta este dolor.
+¿Cómo voy a poder seguir? Mi pasado sigue ahí...
+
+Quiero vivir… siempre y cuando tú estés para mi
+Sálvame esta vez, el final no puede ser.
+
+Hay demonios en mi mente, yo no te olvidaré, donde sea que esté.
+
+Quiero morir… pero si estás, no lo quisiera estar
+Te importé, no es justo de esa forma agradecer
+
+Siempre que lo intenté, no me es suficiente que en mi mente tú estés
+
+Quiero morir
+-ro morir
+-ro morir
+No me importa si aún a mi lado estás
+Por esos días que pasé en soledad.
+Oh quédate… (Oh)
+
+Las heridas jamás cerrarán
+El pasado me seguirá
+Me atormentará una vez más.`,
+
+	"After_Dark.mp3": `I see you, you see me
+How pleasant, this feeling
+The moment you hold me
+I missed you, I'm sorry
+I've given what I have
+I showed you I'm growing
+The ashes fall slowly
+As your voice consoles me
+
+As the hours pass
+I will let you know
+That I need to ask
+Before I'm alone
+How it feels to rest
+On your patient lips
+To eternal bliss
+I'm so glad to know
+
+As the hours pass
+I will let you know
+That I need to ask
+Before I'm alone
+How it feels to rest
+On your patient lips
+To eternal bliss
+I'm so glad to know
+
+We're swaying to drum beats
+In motion, I'm feeling
+My patience controlling
+The question I won't speak
+We're telling the stories
+Our laughter, he knows me
+We're leaving, we're talking
+You're closer, it's calming
+
+As the hours pass
+I will let you know
+That I need to ask
+Before I'm alone
+How it feels to rest
+On your patient lips
+To eternal bliss
+I'm so glad to know
+
+As the hours pass
+I will let you know
+That I need to ask
+Before I'm alone
+How it feels to rest
+On your patient lips
+To eternal bliss
+I'm so glad to know
+
+The night will hold us close and the stars will guide us home
+I've been waiting for this moment, we're finally alone
+I turn to ask the question, so anxious, my thoughts
+Your lips were soft like winter, in your passion, I was lost
+
+As the hours pass
+I will let you know
+That I need to ask
+Before I'm alone
+How it feels to rest
+On your patient lips
+To eternal bliss
+I'm so glad to know`,
+
+	"Amour_Plastique.mp3": `Dans mon esprit tout divague
+Je me perds dans tes yeux
+Je me noie dans la vague
+De ton regard amoureux
+
+Je ne veux que ton âme
+Divaguant sur ma peau
+Une fleur, une femme
+Dans ton cœur Roméo
+
+Je ne suis que ton nom
+Le souffle lancinant
+De nos corps dans le sombre
+Animés lentement
+
+Et la nuit je pleure des larmes
+Qui coulent le long de mes joues
+Je ne pense à toi que quand le jour sombre
+Que s'abattent sur moi
+Mes tristes démons, dans l'abîme sans fond
+Aime-moi jusqu'à ce que les roses fanent
+Que nos âmes sombrent dans les limbes profondes
+
+Et la nuit quand tout est sombre
+Je te regarde danser
+
+Je raisonne en baisers
+Le long de ta poitrine
+Perdu dans l'avalanche
+De mon cœur égaré
+Qui es-tu? Où es-tu?
+Par les pleurs, par les rires
+De ton ambre effarée
+Je raisonne en baisers
+
+Dans mon esprit tout divague
+Je me perds dans tes yeux
+Je me noie dans la vague
+De ton regard amoureux
+
+Je ne veux que ton âme
+Divaguant sur ma peau
+Une fleur, une femme
+Dans ton cœur Roméo
+
+Je ne suis que ton nom
+Le souffle lancinant
+De nos corps dans le
+Sombre animés lentement
+
+Et la nuit quand tout est sombre
+Je te regarde danser`,
+
+	"Amour_Plastique_Español.mp3": `Sólo puedo divagar
+me perdí en tus ojos, hoy.
+Nado en el inmenso mar
+de tu mirada del amor.
+Tu alma divaga en mi piel.
+Sé que hay en tu corazón
+una flor y una mujer, mi querido Romeo.
+Soy ese suspiro que
+late en la obscuridad
+y une nuestros cuerpos hoy
+lentamente y tan vivaz.
+Por la noche derramo lágrimas
+y tan sólo pienso en ti. En los días más sombríos,
+mis tristes demonios vagan por la ciudad sin fondo.
+Ámame hasta que las rosas mueran
+y nuestras almas se hundan entre lágrimas.
+Lalalalala
+En la noche observo 
+como bailas en la obscuridad.
+Lalalalala
+Mmmmm
+
+Resueno entre besos hoy, por todo tu pecho, amor.
+Una avalancha sucedió dentro de mi corazón.
+¿Quién eres tú? ¿Dónde estás?
+El llorar y el reír de tu sombra con temor.
+Resueno entre besos, hoy.
+
+Sólo puedo divagar
+me perdí en tus ojos, hoy.
+Nado en el inmenso mar
+de tu mirada del amor.
+Tu alma divaga en mi piel.
+Sé que hay en tu corazón
+una flor y una mujer, mi querido Romeo.
+Soy ese suspiro que
+late en la obscuridad
+y une a nuestros cuerpos hoy
+lentamente y tan vivaz.
+Lalalalala
+En la noche observo 
+como bailas en la obscuridad.
+Lalalalala`,
+
+	"Another_love.mp3": `I wanna take you somewhere so you know I care
+But it's so cold and I don't know where
+I brought you daffodils in a pretty string
+But they won't flower like they did last spring
+
+And I wanna kiss you, make you feel alright
+I'm just so tired to share my nights
+I wanna cry and I wanna love
+But all my tears have been used up
+
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+Oh, oh
+
+Oh, oh, oh, oh, oh
+Oh, oh, oh, oh, oh
+
+And if somebody hurts you, I wanna fight
+But my hand's been broken one too many times
+So I'll use my voice, I'll be so fucking rude
+Words, they always win, but I know I'll lose
+
+And I'd sing a song that'd be just ours
+But I sang 'em all to another heart
+And I wanna cry, I wanna learn to love
+But all my tears have been used up
+
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+Oh, oh
+
+Ooh, ooh, ooh, ooh, ooh
+Ooh, ooh, ooh, ooh, ooh (oh, need a love now, my heart is thinking of)
+
+I wanna sing a song that'd be just ours
+But I sang 'em all to another heart
+And I wanna cry, I wanna fall in love
+But all my tears have been used up
+
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+On another love, another love
+All my tears have been used up
+
+Oh, oh, oh`,
+
+	"Another_Love_Esapñol.mp3": `Llevarte lejos y me importes más 
+pero hace frio a donde yo iré 
+
+trajes narcisos en un cristal 
+qur no florecen , nunca lo harán
+quiero besarte y sentirte más
+pero mi cuerpo cansado está 
+
+quiero llorar y aprender amar 
+pero mis lágrimas se fueron ya  
+
+con otro amor 
+En otro amor 
+todas mis lagrimas se fueron ya 
+con otro amor 
+En otro amor 
+todas mis lagrimas se fueron ya 
+con otro amor 
+En otro amor
+todas mis lagrimas se fueron ya....
+
+Y si alguien te hiere yo pelearé 
+Aunque mis manos rotas se encuentren ya 
+Alzo mi voz puedo rudo ser 
+Pero las palabras siempre ganarán 
+Quiero cantarte una canción de amor 
+Ya las canté todas a otro corazón 
+Quiero llorar y aprender amar 
+pero mis lagrimas se fueron ya
+
+con otro amor 
+en otro amor 
+todas mis lágrimas se fueron ya 
+con otro amor 
+en otro amor 
+todas mis lágrimas se fueron ya 
+con otro amor 
+En otro amor 
+todas mis lagrimas se fueron ya 
+
+
+Quiero cantarte más 
+otra canción de amor 
+pero ya las canté a otro corazón 
+Quiero llorar y aprender amar 
+pero mis lágrimas se fueron ya 
+
+con otro amor
+En otro amor 
+todas mis lágrimas se fueron ya 
+con otro amor 
+En otro amor 
+todas mis lágrimas se fueron ya 
+con otro amor 
+En otro amor 
+todad mis lágrimas se fueron ya..... `,
+
+	"I_Wanna_Be_Your_Slave.mp3": `I wanna be your slave
+I wanna be your master
+I wanna make your heartbeat
+Run like rollercoasters
+
+I wanna be a good boy
+I wanna be a gangster
+'Cause you can be the beauty
+And I could be the monster
+
+I love you since this morning
+Not just for aesthetic
+I wanna touch your body
+So fucking electric
+
+I know you scared of me
+You said that I'm too eccentric
+I'm crying all my tears
+And that’s fucking pathetic
+
+I wanna make you hungry
+Then I wanna feed you
+I wanna paint your face
+Like you’re my Mona Lisa
+
+I wanna be a champion
+I wanna be a loser
+I’ll even bе a clown
+'Cause I just wanna amuse you
+
+I wanna be your sеx toy
+I wanna be your teacher
+I wanna be your sin
+I wanna be a preacher
+I wanna make you love me
+Then I wanna leave you
+
+'Cause baby I'm your David
+And you’re my Goliath
+
+Because I'm the devil
+Who’s searching for redemption
+And I'm a lawyer
+Who’s searching for redemption
+
+And I'm a killer
+Who’s searching for redemption
+I'm a motherfucking monster
+Who’s searching for redemption
+
+And I'm a bad guy
+Who’s searching for redemption
+And I'm a blond girl
+Who’s searching for redemption
+
+And I'm a freak that
+Is searching for redemption
+I'm a motherfucking monster
+Who’s searching for redemption
+
+I wanna be your slave
+I wanna be your master
+I wanna make your heart beat
+Run like rollercoasters
+
+I wanna be a good boy
+I wanna be a gangster
+'Cause you can be the beauty
+And I could be the monster
+
+I wanna make you quiet
+I wanna make you nervous
+I wanna set you free
+But I'm too fucking jealous
+
+I wanna pull your strings
+Like you’re my telecaster
+And if you want to use me
+I could be your puppet
+
+'Cause I'm the devil
+Who’s searching for redemption
+And I'm a lawyer
+Who’s searching for redemption
+
+And I'm a killer
+Who’s searching for redemption
+I'm a motherfucking monster
+Who’s searching for redemption
+
+I wanna be your slave
+I wanna be your master`,
+
+	"I_Wanna_Be_Your_Slave_Español.mp3": `Yo quiero ser tu esclavo, quiero ser tu amo.
+Quiero tu corazón como montaña rusa
+Quiero ser un buen chico, quiero ser un gangster.
+Tú puedes ser la bella y yo seré tu bestia…
+
+Te amo desde hoy, no es solo tu lindeza.
+Quiero tocar tu cuerpo, eléctrica belleza.
+Me tienes miedo sé y culpas mi rareza.
+No paro de llorar, patética tristeza.
+Yo quiero darte hambre y luego alimentarte.
+Como mi Mona Lisa la cara pintarte.
+Yo quiero ser tu ganador, después perderte.
+Por ti sería un payaso para entretenerte.
+Quiero ser tu juguete, también tu maestro.
+Ser tu peor pecado y yo quien te confiese.
+Hacer que me ames y dejarte sin que pese.
+Bebé yo soy David y tú Goliat te has vuelto.
+
+(Aja~ um~ ajá~)
+
+Porque soy el Diablo que busca redención.
+Soy un Abogado que busca redención.
+Soy un Asesino que busca redención.
+La maldita Aberración que busca tu perdón.
+Soy un Niño Malo que busca redención.
+Y una Chica Rubia que busca redención
+Un completo Loco que busca redención.
+La maldita Aberración que busca tu perdón…
+
+(¡Auh!)
+
+Yo quiero ser tu esclavo, quiero ser tu amo.
+Quiero tu corazón como montaña rusa
+Quiero ser un buen chico, quiero ser un gangster.
+Tú puedes ser la bella y yo seré tu bestia…
+
+Con suavidad callarte y ponerte nerviosa.
+Yo quiero liberarte, pero soy celoso.
+Quiero tirar tus cuerdas como mi guitarra.
+Tú títere seré si tú a gusto me coses.
+
+Porque soy el Diablo que busca redención.
+Soy un Abogado que busca redención.
+Soy un Asesino que busca redención.
+La Maldita Aberración que busca tu perdón.
+
+(¡Auh!)
+
+Quiero ser tu esclavo, quiero ser tu amo…`,
+
+	"Make_you_Mine.mp3": `Well, I will call you darlin' and everything will be okay
+'Cause I know that I am yours and you are mine
+Doesn't matter anyway
+In the night, we'll take a walk, it's nothin' funny
+Just to talk
+
+Put your hand in mine
+You know that I want to be with you all the time
+You know that I won't stop until I make you mine
+You know that I won't stop until I make you mine
+Until I make you mine
+
+Well, I have called you darlin' and I'll say it again, again
+So kiss me 'til I'm sorry, babe, that you are gone and I'm a mess
+And I'll hurt you and you'll hurt me and we'll say things we can't repeat
+
+Put your hand in mine
+You know that I want to be with you all the time
+You know that I won't stop until I make you mine
+You know that I won't stop until I make you mine
+Until I make you mine
+
+You need to know
+We'll take it slow
+I miss you so
+We'll take it slow
+It's hard to feel you slipping (you need to know)
+Through my fingers are so numb (we'll take it slow)
+And how was I supposed to know (I miss you so)
+That you were not the one?
+
+Put your hand in mine
+You know that I want to be with you all the time
+You know that I won't stop until I make you mine
+You know that I won't stop until I make you mine
+Until I make you mine
+
+Put your hand in mine
+You know that I want to be with you all the time
+Oh darlin', darlin', baby, you're so very fine
+You know that I won't stop until I make you mine
+Until I make you`,
+
+	"Make_You_Mine_Español": ``,
+
+};
 const audio = document.getElementById('audio');
 const playBtn = document.getElementById('play-btn');
 const progressContainer = document.getElementById('progress-container');
@@ -69,4 +754,9 @@ function setSong(index) {
   audio.currentTime = 0;
   durationEl.textContent = '0:00';
   currentTimeEl.textContent = '0:00';
+
+  // Mostrar letra
+  const currentSrc = songs[currentSongIndex].getAttribute('data-src');
+  const lyricsDiv = document.getElementById('lyrics');
+  lyricsDiv.textContent = lyrics[currentSrc] || "Letra no disponible para esta canción.";
 }
